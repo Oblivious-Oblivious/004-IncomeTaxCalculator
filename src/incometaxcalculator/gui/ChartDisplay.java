4e882d1b10e7ad406e6d4560e74de7a1f160f37a
Oblivious <thanos.papap42@gmail.com>
2022-12-08 15:29:59 +0200

    Exposed ChartDisplay interface to public.

--- src/incometaxcalculator/gui/ChartDisplay.java
+++ src/incometaxcalculator/gui/ChartDisplay.java
@@ -12,22 +12,7 @@ import org.jfree.data.category.DefaultCategoryDataset;
 import org.jfree.data.general.DefaultPieDataset;
 import org.jfree.ui.RefineryUtilities;
 
-class ChartDisplay {
-
-  static JFrame createPieChart(double entertainment, double basic, double travel, double health,
-      double other) {
-
-    JFrame pieChartFrame = new JFrame("Analysis of receipts");
-    pieChartFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-    pieChartFrame.setLayout(new BorderLayout(0, 5));
-    pieChartFrame.add(createPieChartPanel(entertainment, basic, travel, health, other),
-        BorderLayout.CENTER);
-    pieChartFrame.pack();
-    RefineryUtilities.centerFrameOnScreen(pieChartFrame);
-    pieChartFrame.setVisible(true);
-    return pieChartFrame;
-  }
-
+public class ChartDisplay {
   private static ChartPanel createPieChartPanel(double entertainment, double basic, double travel,
       double health, double other) {
     JFreeChart pieChart = ChartFactory.createPieChart(
@@ -48,17 +33,6 @@ class ChartDisplay {
     pieChartDataset.setValue("Health", health);
     pieChartDataset.setValue("Other", other);
     return pieChartDataset;
-  }
-
-  static JFrame createBarChart(double basicTax, double taxVariation, double totalTax) {
-    JFrame barChartFrame = new JFrame("Bar Chart");
-    barChartFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-    barChartFrame.setLayout(new BorderLayout(0, 5));
-    barChartFrame.add(createBarChartPanel(basicTax, taxVariation, totalTax), BorderLayout.CENTER);
-    barChartFrame.pack();
-    RefineryUtilities.centerFrameOnScreen(barChartFrame);
-    barChartFrame.setVisible(true);
-    return barChartFrame;
   }
 
   private static ChartPanel createBarChartPanel(double basicTax, double taxVariation,
@@ -82,5 +56,30 @@ class ChartDisplay {
     }
     barChartDataset.addValue(totalTax, "Tax", "Total");
     return barChartDataset;
+  }
+
+  public static JFrame createPieChart(double entertainment, double basic, double travel, double health,
+      double other) {
+
+    JFrame pieChartFrame = new JFrame("Analysis of receipts");
+    pieChartFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+    pieChartFrame.setLayout(new BorderLayout(0, 5));
+    pieChartFrame.add(createPieChartPanel(entertainment, basic, travel, health, other),
+        BorderLayout.CENTER);
+    pieChartFrame.pack();
+    RefineryUtilities.centerFrameOnScreen(pieChartFrame);
+    pieChartFrame.setVisible(true);
+    return pieChartFrame;
+  }
+
+  public static JFrame createBarChart(double basicTax, double taxVariation, double totalTax) {
+    JFrame barChartFrame = new JFrame("Bar Chart");
+    barChartFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+    barChartFrame.setLayout(new BorderLayout(0, 5));
+    barChartFrame.add(createBarChartPanel(basicTax, taxVariation, totalTax), BorderLayout.CENTER);
+    barChartFrame.pack();
+    RefineryUtilities.centerFrameOnScreen(barChartFrame);
+    barChartFrame.setVisible(true);
+    return barChartFrame;
   }
 }
