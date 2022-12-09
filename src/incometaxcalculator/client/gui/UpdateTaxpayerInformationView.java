befdaf593d7a697e2a73110c8d7e8b7e34eca31f
Oblivious <thanos.papap42@gmail.com>
2022-12-09 22:24:46 +0200

    Delete UpdateTaxpayerInformationView.java

--- src/incometaxcalculator/client/gui/UpdateTaxpayerInformationView.java
+++
@@ -1,7 +0,0 @@
-package incometaxcalculator.client.gui;
-
-public class UpdateTaxpayerInformationView {
-    public void update(String taxpayer_afm, int receipt_id) {
-        // TODO This is only internal, does not have a GUI interface
-    }
-}
