44609034433709931b462a735514b162d760e887
Oblivious <thanos.papap42@gmail.com>
2022-12-11 17:00:01 +0200

    Finalized selector use case.

--- src/incometaxcalculator/app/select_taxpayer/SelectTaxpayer.java
+++ src/incometaxcalculator/app/select_taxpayer/SelectTaxpayer.java
@@ -1,19 +1,15 @@
 package incometaxcalculator.app.select_taxpayer;
 
-// import incometaxcalculator.app.taxpayers.Taxpayer;
+import incometaxcalculator.app.taxpayers.Taxpayer;
 import incometaxcalculator.boundaries.SelectTaxpayerBoundary;
 import incometaxcalculator.persistence.TaxpayerHashmap;
 
 public class SelectTaxpayer implements SelectTaxpayerBoundary {
-    // Taxpayer current_taxpayer;
-
     @Override
-    // public void select(int tax_registration_number) {
-    public void select() {
-        // this.current_taxpayer = TaxpayerManager.taxpayerHashMap.get(tax_registration_number);
+    public Taxpayer select(int tax_registration_number) {
+        return TaxpayerHashmap.get(tax_registration_number);
     }
 
-    // TODO Eventually perform all calculation here
     public boolean taxpayer_hashmap_is_not_empty() {
         return !TaxpayerHashmap.is_empty();
     }
