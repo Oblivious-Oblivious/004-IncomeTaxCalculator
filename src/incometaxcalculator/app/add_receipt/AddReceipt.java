16386ede0641c7da5f319209d563de77e3e42941
Oblivious <thanos.papap42@gmail.com>
2022-12-12 18:54:46 +0200

    Revert "Update AddReceipt.java"
    This reverts commit cd43870bea760ae205f9da65b2461c0aae94eeb6.

--- src/incometaxcalculator/app/add_receipt/AddReceipt.java
+++ src/incometaxcalculator/app/add_receipt/AddReceipt.java
@@ -14,13 +14,13 @@ import incometaxcalculator.app.taxpayers.Taxpayer;
 
 public class AddReceipt implements AddReceiptBoundary {
     @Override
-    public void add(int receipt_id, String date, float amount, ReceiptKind kind, String company_name, String country, String city, String street, int number, int tax_registration_number) throws IOException, WrongReceiptKindException, WrongReceiptDateException, ReceiptAlreadyExistsException {
-        Taxpayer current_taxpayer = TaxpayerHashmap.get(tax_registration_number);
+    public void add(int receiptId, String issueDate, float amount, ReceiptKind kind, String companyName, String country, String city, String street, int number, int taxRegistrationNumber) throws IOException, WrongReceiptKindException, WrongReceiptDateException, ReceiptAlreadyExistsException {
+        Taxpayer current_taxpayer = TaxpayerHashmap.get(taxRegistrationNumber);
 
-        if(current_taxpayer.receiptHashMap.containsKey(receipt_id))
+        if(current_taxpayer.receiptHashMap.containsKey(receiptId))
             throw new ReceiptAlreadyExistsException();
 
-        current_taxpayer.addReceipt(new Receipt(receipt_id, date, amount, kind, company_name, country, city, street, number));
-        new UpdateTaxpayerInformation().update(tax_registration_number);
+        current_taxpayer.addReceipt(new Receipt(receiptId, issueDate, amount, kind, companyName, country, city, street, number));
+        new UpdateTaxpayerInformation().update(taxRegistrationNumber);
     }
 }
