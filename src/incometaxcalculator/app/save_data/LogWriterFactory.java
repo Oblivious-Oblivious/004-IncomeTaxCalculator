6df9f6a1823954d89af2c997917ab7da738bac23
Oblivious <thanos.papap42@gmail.com>
2022-12-12 02:34:57 +0200

    Used taxpayer as a field in the superclass.

--- src/incometaxcalculator/app/save_data/LogWriterFactory.java
+++ src/incometaxcalculator/app/save_data/LogWriterFactory.java
@@ -3,12 +3,12 @@ package incometaxcalculator.app.save_data;
 import incometaxcalculator.app.exceptions.WrongFileFormatException;
 
 public class LogWriterFactory {
-    public static LogWriter create(LogWriterType type) throws WrongFileFormatException {
+    public static LogWriter create(LogWriterType type, int tax_registration_number) throws WrongFileFormatException {
         switch(type) {
             case TXT:
-                return new TXTLogWriter();
+                return new TXTLogWriter(tax_registration_number);
             case XML:
-                return new XMLLogWriter();
+                return new XMLLogWriter(tax_registration_number);
             default:
                 throw new WrongFileFormatException();
         }
--- src/incometaxcalculator/app/save_data/SaveData.java
+++ src/incometaxcalculator/app/save_data/SaveData.java
@@ -8,7 +8,7 @@ import incometaxcalculator.boundaries.SaveDataBoundary;
 public class SaveData implements SaveDataBoundary {
     @Override
     public void export(int tax_registration_number, LogWriterType type) throws IOException, WrongFileFormatException {
-        LogWriter writer = LogWriterFactory.create(type);
-        writer.generateFile(tax_registration_number);
+        LogWriter writer = LogWriterFactory.create(type, tax_registration_number);
+        writer.generateFile();
     }
 }
