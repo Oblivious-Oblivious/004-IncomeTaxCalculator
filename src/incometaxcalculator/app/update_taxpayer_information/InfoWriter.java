b449b2c36d0c00fbea7109498b5c2f9d95e43c19
Oblivious <thanos.papap42@gmail.com>
2022-12-12 02:36:04 +0200

    Update InfoWriter.java

--- src/incometaxcalculator/app/update_taxpayer_information/InfoWriter.java
+++ src/incometaxcalculator/app/update_taxpayer_information/InfoWriter.java
@@ -2,7 +2,6 @@ package incometaxcalculator.app.update_taxpayer_information;
 
 import java.io.IOException;
 
-// TODO Implement into factory
 public abstract class InfoWriter {
     public abstract void generateFile(int taxRegistrationNumber) throws IOException;
 }
