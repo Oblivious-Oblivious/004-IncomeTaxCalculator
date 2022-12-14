19b02397d3bf2a474558991802dc056e77b53a4d
Oblivious <me262thanos@gmail.com>
2022-12-14 02:59:12 +0200

    Added basic acceptance/integration tests.

---
+++ test/incometaxcalculator/app/SpecHelper.java
@@ -0,0 +1,26 @@
+package incometaxcalculator.app;
+
+import incometaxcalculator.app.taxpayers.HeadOfHouseholdTaxpayer;
+import incometaxcalculator.app.taxpayers.MarriedFilingJointlyTaxpayer;
+import incometaxcalculator.app.taxpayers.MarriedFilingSeparatelyTaxpayer;
+import incometaxcalculator.app.taxpayers.SingleTaxpayer;
+import incometaxcalculator.persistence.TaxpayerHashmap;
+
+public class SpecHelper {
+    public static double MIN_DELTA = 1e-323;
+
+    public static void clear_taxpayer_hashmap() {
+        TaxpayerHashmap.clear();
+    }
+
+    public static void setup_taxpayer_hashmap() {
+        TaxpayerHashmap.put(100000000, new SingleTaxpayer("dummy0", 100000000, 120000));
+        TaxpayerHashmap.put(100000001, new SingleTaxpayer("dummy1", 100000001, 120000));
+        TaxpayerHashmap.put(100000002, new HeadOfHouseholdTaxpayer("dummy2", 100000002, 120000));
+        TaxpayerHashmap.put(100000003, new HeadOfHouseholdTaxpayer("dummy3", 100000003, 120000));
+        TaxpayerHashmap.put(100000004, new MarriedFilingSeparatelyTaxpayer("dummy4", 100000004, 120000));
+        TaxpayerHashmap.put(100000005, new MarriedFilingSeparatelyTaxpayer("dummy5", 100000005, 120000));
+        TaxpayerHashmap.put(100000006, new MarriedFilingJointlyTaxpayer("dummy6", 100000006, 120000));
+        TaxpayerHashmap.put(100000007, new MarriedFilingJointlyTaxpayer("dummy7", 100000007, 120000));
+    }
+}
